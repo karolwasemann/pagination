1b311ac21ae7c3e2bfee3c2d8a0057494f596198
karolwasemann <kw.kwaq@gmail.com>
2022-06-01 08:44:30 +0200

    display first page & logik to change current contacts on page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import stylesheets
 import './style.css';
 
-const users = [
+const contact_list = [
   'Peter Petrelli',
   'Claire Bennet',
   'Hiro Nakamura',
@@ -11,9 +11,38 @@ const users = [
 ];
 
 const list_element = document.querySelector('.contact-container');
-const pagination_element = document.querySelector('.list ul');
+const pagination_element = document.querySelector('.pagination ul');
 
 let current_page = 1;
-let rows = 3;
+let contacts_on_page = 2;
 
-const displayContact = () => {};
+const displayContact = (contacts, page, on_page) => {
+  // list_element.innerHTML = '';
+  page--;
+  let loop_start = page * on_page;
+  let contact_to_display = contacts.slice(loop_start, loop_start + on_page);
+  console.log(contact_to_display);
+  for (let i = 0; i < contact_to_display.length; i++) {
+    const div_element = document.createElement('div');
+    div_element.classList.add(
+      'flex',
+      'cursor-pointer',
+      'my-1',
+      'hover:bg-blue-lightest',
+      'rounded'
+    );
+    const template = `
+  <div class="w-4/5 h-10 py-3 px-1">
+    <p class="hover:text-blue-dark">${contact_to_display[i]}</p>
+  </div>`;
+    div_element.innerHTML = template;
+
+    list_element.appendChild(div_element);
+  }
+};
+
+pagination_element.addEventListener('click', (e) => {
+  console.log(e.target.value);
+});
+
+displayContact(contact_list, current_page, contacts_on_page);
