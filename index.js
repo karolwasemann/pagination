178dd0d0007a8b516a7eba59dec45ceede84e0d6
karolwasemann <kw.kwaq@gmail.com>
2022-06-01 08:12:48 +0200

    Setup

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 // Import stylesheets
 import './style.css';
+
+const users = [
+  'Peter Petrelli',
+  'Claire Bennet',
+  'Hiro Nakamura',
+  'Nathan Petrelli',
+  'Sylar',
+  'Niki Sanders',
+];
+
+const list_element = document.querySelector('.contact-container');
+const pagination_element = document.querySelector('.list ul');
+
+let current_page = 1;
+let rows = 3;
+
+const displayContact = () => {};
